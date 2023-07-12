9102d83bb79f59019cb407a9ac8411d1d4712143
Amanpreet Chatha <amanpreetchatha99@gmail.com>
2023-07-12 13:29:22 -0400

    updates

---
+++ strings/strings/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  strings
+//
+//  Created by user241524 on 7/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let greeting = """
+                            hello everyone
+                            how are you?
+                        
+                        jkhk7
+                        nkl
+                             jhgjgj  kjnkjn
+                        """
+        var greeting1 = "hello\" world\""
+        var greeting2 = ""
+        if greeting2.isEmpty{
+            print("this string is empty")
+        }
+        //print(greeting1)
+        
+        
+        
+    }
+
+
+}
+
