e1c17181f2ecddb53776feabbab4fe3de534a17c
Amanpreet Chatha <amanpreetchatha99@gmail.com>
2023-07-11 23:15:50 -0400

    updates

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -10,18 +10,20 @@ import UIKit
 class ViewController: UIViewController {
 
     var lightOn = true
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .blue
+    }
+    
+    //@IBOutlet var lightButton: UIButton!
     @IBAction func buttonPressed(_ sender: Any) {
         lightOn.toggle()
-        if lightOn {
-            view.backgroundColor = .white
-        } else {
-            view.backgroundColor = .black
-        }
+        updateUI()
         
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         
         
         
