f9b15e9f07c48e19f84e733ac153f7585ec88c7e
Amanpreet Chatha <amanpreetchatha99@gmail.com>
2023-07-11 22:24:51 -0400

    uploading

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        print("light status"  ,lightOn)
+        if lightOn{
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .white
+        }
+        print("light status a tthe end", lightOn)
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
     }
 
 
