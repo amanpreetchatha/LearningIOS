b7ec7f62a59d7884bf4fd30bf44b25f06003610b
Amanpreet Chatha <50677671+amanpreetchatha@users.noreply.github.com>
2023-07-11 20:48:59 -0400

    Add files via upload

---
+++ Light/Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by user241524 on 7/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
